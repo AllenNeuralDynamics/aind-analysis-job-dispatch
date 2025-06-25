d6a0b3c94e9c6f27e9417b0e98cab73042b27aa2
arjunsridhar12345 <109561860+arjunsridhar12345@users.noreply.github.com>
2025-06-24 18:41:59 -0700

    Feat analysis dispatch (#6)
    * adapt to group data asset ids, change output filename. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/741. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/743
    
    * fix group assets, clean up and lint
    
    * update readme
    
    * update readme
    
    * updates for hackathon. simplify args, add separate function for using query
    
    * make query more efficient. fix getting input model information
    
    * add results package dependency and use input model from there
    
    * update readme to reflect model changes
    
    * update readme to reflect changes

--- README.md
+++ README.md
@@ -7,46 +7,38 @@ This script accepts several command-line arguments:
 
 | Argument               | Type    | Description                                                                                                                                             |
 |------------------------|---------|---------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `--query`  | string | The dictionary filter to query the document database or the path to a json containing the query.
 | `--file_extension`      | string  | The file extension to search for from the bucket returned by the query. Defualt is empty                                                                                                             |
 | `--split_files`   | int  | Either group the files into one list if multiple files are returned for the file extension or split into single input per file. Default is to split
 | `--num_parallel_workers`    | int  |  The number of parallel workers to output, default is 50
+| `--use_data_asset_csv`  | int | Whether or not to use the data asset ids in the csv provided. Default is 0. If 1, there MUST be a csv in the `/data` folder called `data_asset_input.csv`, with the column `asset_id`.
 
-### Example Command:
 
-```bash
-python script.py \
-    --file_extension "" \
-    --split_files 1 \
-    --num_parallel_workers 50
-```
-
-### Specifying Necessary Contents
-There are 2 required user defined variables that need to be set in the code. They are both found in `code/job_dispatch/run_capsule.py`. The first of these is either the query or list of data assets. Look in the main section of `run_capsule.py` and replace with desired query or list of data assets to get input model.
-
-The last necessary thing is to define the input analysis specification. This is in the function `get_analysis_specifications()` in `run_capsule.py`. Navigate to this function and create your desired analysis specifications and append to the list. The output of the dispatcher will be a json for every analysis-input-data combination. See example content below.
+If you want to group the data assets into certain groupings, see portion of code in `run_capsule` in line 216 where you can specify the grouping you desire.
 
 ### Example Output File Content
 
-The content of the JSON file will look something like this (it will be saved as bucket_name.json):
+For parallelization, the output will be a folder for each worker. The output will be a json model (model lives here [Dispatch Analysis Model](https://github.com/AllenNeuralDynamics/aind-analysis-results/blob/main/src/aind_analysis_results/analysis_dispatch_model.py) with a uuid as the filename. Example content shown below.
 
 ```json
 {
-    "s3_location": "s3://codeocean-s3datasetsbucket-1u41qdg42ur9/d94ab360-f393-41f4-831f-e098f11803df",
-    "location_asset_id": "d94ab360-f393-41f4-831f-e098f11803df",
-    "location_uri": null,
-    "asset_name": "behavior_741213_2024-08-26_16-28-28_processed_2024-09-17_07-25-04",
-    "parameters": {
-        "analysis_name": "Unit Yield",
-        "analysis_version": "v0.0.1",
-        "analysis_libraries_to_track": [
-            "aind-ephys-utils"
-        ],
-        "analysis_parameters": {
-            "isi_violations": 0.5
-        }
-    }
+    "s3_location": [
+        "s3://codeocean-s3datasetsbucket-1u41qdg42ur9/50fa9416-4e21-482f-8901-889322a87ae3"
+    ],
+    "asset_id": [
+        "50fa9416-4e21-482f-8901-889322a87ae3"
+    ],
+    "asset_name": [
+        "behavior_774659_2025-06-07_14-31-15_processed_2025-06-08_03-49-49"
+    ],
+    "file_location": [
+        "s3://codeocean-s3datasetsbucket-1u41qdg42ur9/50fa9416-4e21-482f-8901-889322a87ae3/nwb/behavior_774659_2025-06-07_14-31-15.nwb"
+    ]
 }
 ```
+
+```
+
 
 ### Metadata Query Generator (In development)
 The linked portal helps generate simple queries that can be used as input into the job dispatch. Currently under development, but example query from filling out fields: `{"data_description.project_name": "Ephys Platform", "subject.subject_id": {"$in": ["643634"]}}`.
--- code/job_dispatch/run_capsule.py
+++ code/job_dispatch/run_capsule.py
@@ -5,7 +5,8 @@ Generates the input analysis model from the user provided query
 import argparse
 import json
 import logging
-import pathlib
+from pathlib import Path
+import uuid
 from typing import Union
 
 import numpy as np
@@ -13,7 +14,7 @@ import pandas as pd
 from tqdm import tqdm
 
 from job_dispatch import utils
-from job_dispatch.analysis_input_model import InputAnalysisModel
+from aind_analysis_results.analysis_dispatch_model import AnalysisDispatchModel
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +32,7 @@ def get_input_parser() -> argparse.ArgumentParser:
     argparse.ArgumentParser
         A configured ArgumentParser object with predefined command-line arguments for:
         - `--docdb_query`: A json string of query for getting data assets
-        - `--use_data_assets`: If true, read in list of data asset ids and use those instead of query
+        - `--use_data_asset_csv`: If true, read in list of data asset ids and use those instead of query
         - `--file_extension`: A string argument for specifying whether or not to find the file extension
         - `--split_files`: Whether or not to group the files into a single model or to split into seperate
         - `--num_parallel_workers`: The number of parallel workers to output, default is 50. Determined by min(length of results returned in query, 50).
@@ -39,8 +40,8 @@ def get_input_parser() -> argparse.ArgumentParser:
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--docdb_query', type=str, default="")
-    parser.add_argument('--use_data_assets', type=int, default=0)
+    parser.add_argument("--docdb_query", type=str, default="")
+    parser.add_argument("--use_data_asset_csv", type=int, default=0)
     parser.add_argument("--file_extension", type=str, default="")
     parser.add_argument("--split_files", type=int, default=1)
     parser.add_argument("--num_parallel_workers", type=int, default=50)
@@ -48,23 +49,19 @@ def get_input_parser() -> argparse.ArgumentParser:
     return parser
 
 
-
 def get_input_model_list(
-    docdb_query: Union[dict, None] = None,
-    data_asset_ids: Union[list[str], None] = None,
+    data_asset_ids: Union[list[str], list[list[str]]],
     file_extension: str = "",
     split_files: bool = True,
-) -> list[InputAnalysisModel]:
+) -> list[AnalysisDispatchModel]:
     """
     Writes the input model with the S3 location from the query and input arguments
 
     Parameters
     ----------
-    analysis_specifications: list[AnalysisSpecification]
-        The list of analysis to run on data 
 
-    docdb_query : dict
-        A dictionary representing the query to retrieve documents from the database.
+    data_asset_ids: Union[list[str], list[list[str]], None]
+        The data asset ids to get input models for. Either a flat list or nested list of lists.
 
     file_extension : str, optional
         The file extension to filter for when searching the S3 locations. Defaults to empty, meaning the bucket path will be returned from the query.
@@ -74,50 +71,59 @@ def get_input_model_list(
 
     Returns
     -------
-    list: InputAnalysisModel
+    list: AnalysisDispatchModel
         Returns a list of input analysis jobs
     """
-    if docdb_query is None and data_asset_ids is None:
-        raise ValueError(
-            "Either one of docdb query or list of data asset ids must be provided"
+
+    # Normalize to grouped format
+    is_flat = True
+    if isinstance(data_asset_ids, list) and all(
+        isinstance(i, str) for i in data_asset_ids
+    ):
+        logger.info("Flat data asset ids list provided")
+        grouped_asset_ids = [data_asset_ids]
+    elif isinstance(data_asset_ids, list) and all(
+        isinstance(i, list) for i in data_asset_ids
+    ):
+        logger.info("Nested data asset ids list provided")
+        grouped_asset_ids = data_asset_ids
+        is_flat = False
+
+    all_grouped_models = []
+
+    for group in grouped_asset_ids:
+        s3_buckets, s3_asset_ids, s3_paths, s3_asset_names = utils.get_s3_input_information(
+            data_asset_ids=group,
+            file_extension=file_extension,
+            split_files=split_files,
         )
 
-    s3_buckets, s3_asset_ids, s3_paths, s3_asset_names = utils.get_s3_file_locations(
-        query=docdb_query,
-        data_asset_ids=data_asset_ids,
-        file_extension=file_extension,
-        split_files=split_files,
-    )
-
-    input_model_jobs = []
-
-    for index, bucket in enumerate(s3_buckets):
-        s3_asset_id = s3_asset_ids[index]
-        s3_asset_name = s3_asset_names[index]
-        s3_bucket_paths = None
-        if s3_paths:
-            s3_bucket_paths = s3_paths[index]
-
-        if s3_bucket_paths is not None:
-            input_analysis_model = InputAnalysisModel(
-                s3_location=bucket,
-                location_asset_id=s3_asset_id,
-                file_extension_locations=s3_bucket_paths,
-                asset_name=s3_asset_name,
-            )
+        if is_flat:
+            for index, s3_bucket in enumerate(s3_buckets):
+                all_grouped_models.append(
+                    AnalysisDispatchModel(
+                        s3_location=[s3_bucket],
+                        asset_id=[s3_asset_ids[index]],
+                        file_location=[s3_paths[index]] if s3_paths else None,
+                        asset_name=[s3_asset_names[index]],
+                    )
+                )
         else:
-            input_analysis_model = InputAnalysisModel(
-                s3_location=bucket,
-                location_asset_id=s3_asset_id,
-                asset_name=s3_asset_name,
+            all_grouped_models.append(
+                AnalysisDispatchModel(
+                    s3_location=s3_buckets,
+                    asset_id=s3_asset_ids,
+                    file_location=s3_paths if s3_paths else None,
+                    asset_name=s3_asset_names,
+                )
             )
-        input_model_jobs.append(input_analysis_model)
 
-    return input_model_jobs
+    return all_grouped_models
 
 
 def write_input_model_list(
-    input_model_list: list[InputAnalysisModel], num_parallel_workers: int
+    input_model_list: list[AnalysisDispatchModel],
+    num_parallel_workers: int,
 ) -> None:
     """
     Distributes a list of input models across a specified number of parallel workers,
@@ -125,8 +131,8 @@ def write_input_model_list(
 
     Parameters
     ----------
-    input_model_list : list of InputAnalysisModel
-        A list of InputAnalysisModel instances to be processed and written to disk.
+    input_model_list : list of AnalysisDispatchModel
+        A list of AnalysisDispatchModel instances to be processed and written to disk.
 
     num_parallel_workers : int
         The maximum number of parallel workers that can be used to process the input models.
@@ -137,29 +143,63 @@ def write_input_model_list(
         This function does not return any value. It writes JSON files to disk for each input model.
     """
 
+    # Step 1: Split into worker batches
     num_actual_workers = min(len(input_model_list), num_parallel_workers)
     jobs_for_each_worker = np.array_split(input_model_list, num_actual_workers)
 
-    for n_worker, input_model_jobs in tqdm(
-        enumerate(jobs_for_each_worker),
-        desc="Assigning jobs to workers",
-        total=len(jobs_for_each_worker),
+    # Step 2: Write output per job inside worker folder
+    for worker_id, job_group in enumerate(
+        tqdm(jobs_for_each_worker, desc="Distributing jobs")
     ):
-        results_directory_worker_path = utils.RESULTS_PATH / f"{n_worker}"
-        if not results_directory_worker_path.exists():
-            results_directory_worker_path.mkdir()
+        worker_folder = utils.RESULTS_PATH / f"{worker_id}"
+        worker_folder.mkdir(parents=True, exist_ok=True)
 
-        for input_model_job in input_model_jobs:
-            with open(
-                results_directory_worker_path
-                / f"{input_model_job.location_asset_id}.json",
-                "w",
-            ) as f:
-                f.write(input_model_job.model_dump_json(indent=4))
+        for job_id, job_model in enumerate(job_group):
+            # Write input model
+            with open(worker_folder / f"{uuid.uuid4()}.json", "w") as f:
+                f.write(job_model.model_dump_json(indent=4))
 
-        logger.info(
-            f"{len(input_model_jobs)} input analysis models written to {utils.RESULTS_PATH} for worker {n_worker}"
-        )
+        logger.info(f"{len(job_group)} jobs written to {worker_folder}")
+
+
+def get_data_asset_ids(args) -> list[str]:
+    """
+    Retrieve a list of data asset IDs based on the provided arguments.
+
+    Parameters
+    ----------
+    args : input arguments for determining which input to use - query or list of ids
+
+    Returns
+    -------
+    list of str
+        A list of data asset ID strings that match the provided filters.
+    """
+    if bool(args.use_data_asset_csv):
+        data_asset_ids_path = tuple(utils.DATA_PATH.glob("*.csv"))
+        if not data_asset_ids_path:
+            raise FileNotFoundError("Using data asset ids, but no path to csv provided")
+
+        data_asset_df = pd.read_csv(data_asset_ids_path[0])
+        if data_asset_df["asset_id"].isna().all():
+            raise ValueError("Asset id column is empty")
+
+        data_asset_ids = data_asset_df["asset_id"].tolist()
+
+    if args.docdb_query and not bool(args.use_data_asset_csv):
+        logger.info("Using query")
+        if isinstance(args.docdb_query, str) and Path(args.docdb_query).exists():
+            logger.info(f"Query input as json file at path {Path(args.docdb_query)}")
+            with open(Path(args.docdb_query), "r") as f:
+                query = json.load(f)
+        else:
+            query = json.loads(args.docdb_query)
+
+        logger.info(f"Query {query}")
+        data_asset_ids = utils.get_data_asset_ids_from_query(query)
+
+    logger.info(f"Returned {len(data_asset_ids)} records")
+    return data_asset_ids
 
 
 if __name__ == "__main__":
@@ -172,26 +212,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logger.info(args)
 
-    query = None
-    data_asset_ids = None
-
-    if bool(args.use_data_assets):
-        data_asset_ids_path = tuple(utils.DATA_PATH.glob("*.csv"))
-        if not data_asset_ids_path:
-            raise FileNotFoundError("Using data asset ids, but no path to csv provided")
-        
-        data_asset_df = pd.read_csv(data_asset_ids_path[0])
-        if data_asset_df["asset_id"].isna().all():
-            raise ValueError("Asset id column is empty")
-        
-        data_asset_ids = data_asset_df["asset_id"].tolist()
-            
-    if args.docdb_query and not bool(args.use_data_assets):
-        query = json.loads(args.docdb_query)
-    
+    data_asset_ids = get_data_asset_ids(args)
+    ### IF YOU WANT TO GROUP DATA ASSETS, REPLACE THIS TO GET GROUPED IDS
+    ### OR RESTRUCTURE FLAT LIST INTO GROUPS
 
     input_model_list = get_input_model_list(
-        docdb_query=query,
         data_asset_ids=data_asset_ids,
         file_extension=args.file_extension,
         split_files=bool(args.split_files),
